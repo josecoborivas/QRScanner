e12c709225cf1290b5614f3b202d03c0c2745992
josecoborivas <josecoborivas@gmail.com>
2020-06-24 19:23:24 -0300

    history

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import { DataLocalService } from '../../services/data-local.service';
 
 @Component({
   selector: 'app-tab1',
@@ -12,7 +13,7 @@ export class Tab1Page {
     allowSlidePrev: false,
     allowSlideNext: false
   }
-  constructor(private barcodeScanner: BarcodeScanner) {}
+  constructor(private barcodeScanner: BarcodeScanner, private dataLocalService: DataLocalService) {}
 
   //Ciclos de vida de los componentes en Ionic
   ionViewDidEnter(){
@@ -35,8 +36,14 @@ export class Tab1Page {
   scan(){
     this.barcodeScanner.scan().then(barcodeData => {
       console.log('Barcode data', barcodeData);
+
+      if(!barcodeData.cancelled){
+        this.dataLocalService.guardarRegistro(barcodeData.format, barcodeData.text);
+      }
      }).catch(err => {
          console.log('Error', err);
+
+         this.dataLocalService.guardarRegistro('QRScann', 'http://fernando-herrera.com');
      });
   }
 }
--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataLocalService } from '../../services/data-local.service';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,14 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(public dataLocalService: DataLocalService) {}
 
+
+  enviarCorreo(){
+
+  }
+
+  abrirRegistro(registro){
+    console.log('Registro: ', registro);
+  }
 }
